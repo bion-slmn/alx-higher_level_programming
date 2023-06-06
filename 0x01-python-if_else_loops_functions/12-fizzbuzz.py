564155239aa3bffc4cb7ba46396876a66076f001
bion-slmn <bionsol25@gmail.com>
2023-06-06 13:43:55 -0700

    fizz buzz

---
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+# fizzbuzz -  prints the numbers from 1 to 100 separated by a space.
+#  multiples of three print Fizz  and for multiples of five print Buzz.
+# numbers which are multiples of both three and five print FizzBuzz
+
+def fizzbuzz():
+    for x in range(0, 101):
+        if x % 3 == 0 and x % 5 == 0:
+            print("{}".format("FizzBuzz"), end=" ")
+        elif x % 3 == 0:
+            print("{}".format("Fizz"), end=" ")
+        elif x % 5 == 0:
+            print("{}".format("Buzz"), end=" ")
+        else:
+            print("{}".format(x), end=" ")
