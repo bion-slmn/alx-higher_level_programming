9de8a0fce7a88a422e056103741479761a945ed7
bion-slmn <bionsol25@gmail.com>
2023-06-12 12:41:15 -0700

    length -1 update

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -10,7 +10,7 @@ def new_in_list(my_list, idx, element):
     length = len(my_list)
     newList = my_list[:]
 
-    if idx < 0 or idx > length:
+    if idx < 0 or idx > length - 1:
         return (newList)
     else:
         newList[idx] = element
