a33419017eb52fca2728a61b491d5705feaad001
bion-slmn <bionsol25@gmail.com>
2023-06-12 12:23:58 -0700

    adjusting if statement

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -7,7 +7,7 @@
 def element_at(my_list, idx):
     length = len(my_list)
 
-    if idx < 0 or idx > length:
-        return None
-    else:
+    if idx >= 0 or idx <= length - 1:
         return (my_list[idx])
+    else:
+        return None
