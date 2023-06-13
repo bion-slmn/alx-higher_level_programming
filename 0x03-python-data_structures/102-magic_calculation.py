b30b9f0b273029d3ea629bcc3caa45335ff378ff
bion-slmn <bionsol25@gmail.com>
2023-06-13 12:03:43 -0700

    byte code

---
+++ 0x03-python-data_structures/102-magic_calculation.py
@@ -0,0 +1,17 @@
+def magic_calculation(a, b):
+    add
+    sub
+
+    import magic_calculation_102
+    add = magic_calculation_102.add
+    sub = magic_calculation_102.sub
+
+    if (a < b):
+        c = add(a, b)
+
+        for i in range(4, 6):
+            c = add(c, i)
+
+        return c
+    else:
+        sub(a, b)
