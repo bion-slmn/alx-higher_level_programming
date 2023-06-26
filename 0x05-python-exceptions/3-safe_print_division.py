f92afc49d0fbf281586819c3efe1d266f6c9adde
bion-slmn <bionsol25@gmail.com>
2023-06-26 07:05:51 -0700

    zero division error and other errors

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -3,7 +3,7 @@ def safe_print_division(a, b):
     '''divides 2 integers and prints the result.'''
     try:
         result = a / b
-    except ZeroDivisionError:
+    except Exception:
         result = None
     finally:
         print("{}".format(result))
