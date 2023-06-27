ce8e423534c7b69228d9dab3276018ff351d88a1
bion-slmn <bionsol25@gmail.com>
2023-06-27 07:59:15 -0700

    printing squares

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python3
+'''define a class square'''
+
+
+class Square:
+    '''The square is defined by:
+    Private instance attribute: size
+    Instantiation with optional size'''
+    def __init__(self, size=0):
+        '''Initialize a new Square.
+
+        Args:
+            size (:obj:`int`, optional): is the size of a square
+        '''
+        self.__size = size
+
+    @property
+    def size(self):
+        '''Retrive the size value
+
+        Return:
+            the size private instance)'''
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError('size must be an integer')
+        elif value < 0:
+            raise ValueError('size must be >= 0')
+        else:
+            self.__size = value
+
+    def area(self):
+        '''calculates the area of a square
+
+        Args:
+            size (int): the size of the square
+
+        Return:
+            int: the area of the square'''
+        return self.__size ** 2
+
+    def my_print(self):
+        ''' that prints in stdout the square with the character #'''
+        if self.__size == 0:
+            print()
+        else:
+            for x in range(self.__size):
+                for y in range(self.__size):
+                    print("#", end="")
+                print()
