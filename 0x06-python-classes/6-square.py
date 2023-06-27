fccc318c7f7bf3c4a348b934b01238ab22615315
bion-slmn <bionsol25@gmail.com>
2023-06-27 14:19:00 -0700

    addinf newline

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -64,13 +64,13 @@ class Square:
     def my_print(self):
         ''' that prints in stdout the square with the character #'''
         if self.__size == 0:
-            print()
+            print("")
         else:
             for x in range(self.__position[1]):
-                print()
+                print("")
             for x in range(self.__size):
                 for q in range(self.__position[0]):
                     print(" ", end="")
                 for y in range(self.__size):
                     print("#", end="")
-                print()
+                print("")
