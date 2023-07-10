fd7b4065ae27330da57258e212e549f1aa6d5fd3
bion-slmn <bionsol25@gmail.com>
2023-07-10 09:11:42 -0700

    doctest

---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+'''empty class BaseGeometry.'''
+
+
+class BaseGeometry:
+    '''This is an empty class'''
+
+    def area(self):
+        '''This raises an exception'''
+        raise Exception('area() is not implemented')
+
+    def integer_validator(self, name, value):
+        '''validates if value is an int
+        Args:
+            name(string): name of basegeo
+            value(int): must be an int
+        '''
+        if not isinstance(value, int):
+            raise TypeError(f'{name} must be an integer')
+        if value <= 0:
+            raise ValueError(f'{name} must be greater than 0')
