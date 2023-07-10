f200d9911add78cb365d5b09904f20530bd475ef
bion-slmn <bionsol25@gmail.com>
2023-07-10 07:34:28 -0700

    tought

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -6,5 +6,14 @@ from the specified class ; otherwise False.'''
 
 def inherits_from(obj, a_class):
     '''retuns true if obj is directly or indirectly inherited
-    from the specified class'''
+    from the specified class
+
+     Args:
+        obj (any): The object to check.
+        a_class (type): The class to match the type of obj to.
+
+    Returns:
+        If obj is an inherited instance of a_class - True.
+        Otherwise - False.
+        '''
     return issubclass(type(obj), a_class) and type(obj) != a_class
