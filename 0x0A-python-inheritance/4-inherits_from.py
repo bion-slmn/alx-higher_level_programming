a7fd09760e86ce5b46af9ac1e7387a7ffcfb3f6e
bion-slmn <bionsol25@gmail.com>
2023-07-10 07:45:22 -0700

    we woek

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,12 +1,9 @@
 #!/usr/bin/python3
-''' a function that returns True if the object is an instance
-of a class that inherited (directly or indirectly)
-from the specified class ; otherwise False.'''
+''' a function that returns True if the object is an parent'''
 
 
 def inherits_from(obj, a_class):
     '''retuns true if obj is directly or indirectly inherited
-    from the specified class
 
      Args:
         obj (any): The object to check.
