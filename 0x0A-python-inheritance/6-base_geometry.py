4deaf61f6626a2089a64a8c0d2c92c3ae40be4c8
bion-slmn <bionsol25@gmail.com>
2023-07-10 07:57:31 -0700

    raise exception

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+'''empty class BaseGeometry.'''
+
+
+class BaseGeometry:
+    '''This is an empty class'''
+
+    def area(self):
+        '''This raises an exception'''
+        raise Exception('area() is not implemented')
