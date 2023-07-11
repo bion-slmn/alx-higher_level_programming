eda5131ddc4a160311b8a99fcd90e6f0a0d5dcb3
bion-slmn <bionsol25@gmail.com>
2023-07-11 07:52:10 -0700

    load and save

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,13 +1,19 @@
 #!/usr/bin/python3
 '''this module reads from commandline and saves i json fiile'''
 import sys
-save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 
-number_arg = len(sys.argv)
-newlist = load_from_json_file('add_item.json')
+if __name__ == "__main__":
+    save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+    load_from_json_file =\
+        __import__('6-load_from_json_file').load_from_json_file
 
-for x in range(1, number_arg):
-    newlist.append(sys.argv[x])
-save_to_json_file(newlist, 'add_item.json')
+    number_arg = len(sys.argv)
+    try:
+        newlist = load_from_json_file('add_item.json')
+    except Exception:
+        newlist = []
+
+    for x in range(1, number_arg):
+        newlist.append(sys.argv[x])
+    save_to_json_file(newlist, 'add_item.json')
