9f2b6a98530d44d70d398d23136603b410891210
bion-slmn <bionsol25@gmail.com>
2023-07-11 07:41:17 -0700

    load, add

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -6,13 +6,8 @@ load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 
 number_arg = len(sys.argv)
-
 newlist = load_from_json_file('add_item.json')
 
-# create a newlist
 for x in range(1, number_arg):
     newlist.append(sys.argv[x])
-
-# save to the json file
 save_to_json_file(newlist, 'add_item.json')
-# load from the json file
