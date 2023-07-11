b4de37ad0e59144c89bf19a262e80b45777fa18c
bion-slmn <bionsol25@gmail.com>
2023-07-11 05:38:01 -0700

    jzon strings

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3
-import json
 '''This module contains a function that converts an obj tojson string'''
+import json
 
 
 def to_json_string(my_obj):
-    '''converts python obj to json string
-    Args:
-        my_obj(object): python object to be converted to json'''
+    '''converts python obj to json string'''
     return json.dumps(my_obj)
