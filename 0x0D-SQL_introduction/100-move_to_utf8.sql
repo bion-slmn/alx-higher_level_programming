7ecdbffbd9d80f4b13e533530633a9d706c6c91d
bion-slmn <bionsol25@gmail.com>
2023-08-15 07:55:44 -0700

    100

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -13,6 +13,7 @@ COLLATE utf8mb4_unicode_ci;
 
 -- Convert the name field within first_table to UTF8
 ALTER TABLE first_table 
-MODIFY name VARCHAR(256) 
+CHANGE name name
+VARCHAR(256) 
 CHARACTER SET utf8mb4 
 COLLATE utf8mb4_unicode_ci;
