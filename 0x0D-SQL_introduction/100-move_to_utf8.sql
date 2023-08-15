773c5c01247667feccffb3d9b4d639a57979e1ac
bion-slmn <bionsol25@gmail.com>
2023-08-15 08:06:06 -0700

    10001

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,19 +1,4 @@
--- Select the hbtn_0c_0 database
-USE hbtn_0c_0;
-
--- Convert the hbtn_0c_0 database to UTF8
-ALTER DATABASE `hbtn_0c_0` 
-	CHARACTER SET = utf8mb4 
-	COLLATE = utf8mb4_unicode_ci;
-
--- Convert the first_table table to UTF8
-ALTER TABLE `first_table` 
-	CONVERT TO CHARACTER SET utf8mb4 
-	COLLATE utf8mb4_unicode_ci;
-
--- Convert the name field within first_table to UTF8
+-- Converts the entire database hbtn_0c_0 to UTF8.
+USE `hbtn_0c_0`
 ALTER TABLE `first_table`
-	MODIFY `name`
-	VARCHAR(256) 
-	CHARACTER SET utf8mb4 
-	COLLATE utf8mb4_unicode_ci;
+CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
