6eb68571ca979985873cab7c1391327d38981ed7
bion-slmn <bionsol25@gmail.com>
2023-08-16 08:04:45 -0700

    foreign keys

---
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -0,0 +1,15 @@
+-- creates the database hbtn_0d_usa and the table cities (in the database hbtn_0d_usa
+-- create database
+CREATE DATABASE
+    IF NOT EXISTS `hbtn_0d_usa`;
+
+-- Create table state inthe database
+USE `hbtn_0d_usa`;
+CREATE TABLE
+    IF NOT EXISTS `cities` (
+	`id`INT AUTO_INCREMENT NOT NULL,
+	state_id INT NOT NULL,
+	name VARCHAR(256) NOT NULL,
+	PRIMARY KEY (`id`),
+	FOREIGN KEY (state_id) REFERENCES states(`id`)
+	);
