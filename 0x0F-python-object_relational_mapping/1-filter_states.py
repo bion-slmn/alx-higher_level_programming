7737241070d1ad7260ce28814f3c3c236b6ae522
bion-slmn <bionsol25@gmail.com>
2023-09-15 10:42:21 -0700

    length issue

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -10,9 +10,4 @@ if __name__ == '__main__':
                          password=sys.argv[2], db=sys.argv[3])
     cur = db.cursor()
     cur.execute('SELECT * FROM states WHERE name LIKE "N%" ORDER BY states.id')
-    rows = cur.fetchall()
-    for row in rows:
-        print(row)
-
-    cur.close()
-    db.close()
+    [print(row) for row in cur.fetchall()]
