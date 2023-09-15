3f843d36da5a942266e6dea65604ae3b42b36bf1
bion-slmn <bionsol25@gmail.com>
2023-09-15 10:29:10 -0700

    adding port no

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -6,7 +6,7 @@ import sys
 
 
 if __name__ == '__main__':
-    db = MySQLdb.connect(host='localhost', user=sys.argv[1],
+    db = MySQLdb.connect(host='localhost', port=3306, user=sys.argv[1],
                          password=sys.argv[2], db=sys.argv[3])
     cur = db.cursor()
     cur.execute('SELECT * FROM states WHERE name LIKE "N%" ORDER BY states.id')
