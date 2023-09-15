8f5aca2510afdd77e548995fc156fdecb31132b3
bion-slmn <bionsol25@gmail.com>
2023-09-15 14:34:05 -0700

    adding in a one to many relationship

---
+++ 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+'''This module lists all State objects from the database hbtn_0e_6_usa'''
+from model_state import Base, State
+from model_city import City
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine
+import sys
+from sqlalchemy import bindparam
+
+
+if __name__ == '__main__':
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]))
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    state = State(name='California')
+    city = City(name='San Francisco')
+    state.cities.append(city)
+    session.add(state)
+    session.commit()
+    session.close()
---
+++ 0x0F-python-object_relational_mapping/relationship_state.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+'''This module has class definition of a State and
+an instance Base = declarative_base()'''
+
+
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String
+from sqlalchemy import create_engine
+from sqlalchemy.orm import relationship
+import sys
+
+
+Base = declarative_base()
+
+# db = MySQLdb.connect(host='localhost', port=3306, user=sys.argv[1],
+#                     password=sys.argv[2], db=sys.argv[3])
+
+
+class State(Base):
+    '''This defines a class state that links to a table
+    in the database'''
+    __tablename__ = 'states'
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    name = Column(String(128), nullable=False)
+    cities = relationship('City', back_populates='state')
