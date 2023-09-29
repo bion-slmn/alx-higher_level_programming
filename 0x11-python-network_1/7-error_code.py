ec3ae75b90b2064878fa2024f67d7cdb9870abd8
bion-slmn <bionsol25@gmail.com>
2023-09-29 17:09:52 +0300

    error code

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -9,5 +9,6 @@ if __name__ == "__main__":
     try:
         r = requests.get(sys.argv[1])
         r.raise_for_status()
+        print(r.text)
     except requests.exceptions.HTTPError as e:
         print(f'Error code: {e.status_code}')
