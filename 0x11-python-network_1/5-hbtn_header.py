4cd713346a485f4527009e186ecffb22b068d6bf
bion-slmn <bionsol25@gmail.com>
2023-09-29 16:18:37 +0300

    using reqeusts to get id in header

---
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+'''Python script that takes in a URL, sends a request to the URL and displays
+    the value of the X-Request-Id '''
+import sys
+import requests
+
+if __name__ == "__main__":
+    r = requests.get(sys.argv[1])
+    print(r.headers['X-Request-Id'])
