833977f594440fd8b6146e36d39e69d2f7a9576f
bion-slmn <bionsol25@gmail.com>
2023-09-29 14:04:32 +0300

    adding tab

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -7,7 +7,7 @@ import urllib.request
 if __name__ == "__main__":
     with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as r:
         response = r.read()
-        print(f'''Body response:
-    -type: {type(response)}
-    -content: {response}
-    -utf8 content: {response.decode('utf-8')}''')
+        print('Body response:')
+        print(f'\t- type: {type(response)}')
+        print(f'\t- content: {response}')
+        print(f"\t- utf8 content: {response.decode('utf-8')}")
