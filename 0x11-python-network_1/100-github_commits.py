06107e02b09ade3fec2b85b8005f441b2fdd84d0
bion-slmn <bionsol25@gmail.com>
2023-09-29 21:02:20 +0300

    errors

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -2,7 +2,6 @@
 '''Python script that takes your GitHub credentials
 (username and password) and uses the GitHub API to display your id'''
 import requests
-from requests.auth import HTTPBasicAuth
 import sys
 
 
