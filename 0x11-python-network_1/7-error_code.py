64ffa75e3e91dd3319318e89e38c5c6af407b689
bion-slmn <bionsol25@gmail.com>
2023-09-29 17:04:55 +0300

    catching exception using request

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+'''Python script that takes in a URL, sends a request to the
+    URL and displays the body of the response '''
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    try:
+        r = requests.get(sys.argv[1])
+        r.raise_for_status()
+    except requests.exceptions.HTTPError as e:
+        print(f'Error code: {e.status_code}')
