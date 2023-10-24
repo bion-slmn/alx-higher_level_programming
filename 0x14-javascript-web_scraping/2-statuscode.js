617dce8b9139c694ac72bb0f00ea72bf933fde7f
bion-slmn <bionsol25@gmail.com>
2023-10-24 15:48:48 +0300

    getting the status code

---
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+
+const request = require('request');
+request(process.argv[2], (error, response, body) => {
+  if (error) throw error;
+  console.log(`code: ${response.statusCode}`);
+});
