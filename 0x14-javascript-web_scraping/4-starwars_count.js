ba2f902b60405dfebf14a8fcf3f84225eb27f877
bion-slmn <bionsol25@gmail.com>
2023-10-24 17:45:42 +0300

    reduced line count

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,12 +1,10 @@
 #!/usr/bin/node
 const request = require('request');
-const url = process.argv[2];
-const userId = 18;
 
-request(url, (error, response, body) => {
+request(process.argv[2], (error, response, body) => {
   if (error) throw error;
   const jResp = JSON.parse(body);
-  const userUrl = jResp.results[0].characters[0].replace(/\d+/, userId);
+  const userUrl = jResp.results[0].characters[0].replace(/\d+/, 18);
 
   request(userUrl, (error, response, body) => {
     if (error) throw error;
