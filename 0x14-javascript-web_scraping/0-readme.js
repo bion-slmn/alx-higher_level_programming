7c93059693c028d53eceeb0029f355101c15d670
bion-slmn <bionsol25@gmail.com>
2023-10-24 14:29:54 +0300

    less verbose

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,13 +1,7 @@
 #!/usr/bin/node
-
 // this script reads and prints the content of a file.
 const fs = require('fs');
 const fileName = process.argv[2];
-
 fs.readFile(fileName, 'utf-8', function (err, data) {
-  if (data) {
-    console.log(data);
-  } else {
-    console.log(err);
-  }
+  console.log(err || data);
 });
